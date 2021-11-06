8dd81e7ee71a70597f5a224b547763d7be36809b
yavorant <yavorant@gmail.com>
2021-11-07 00:28:28 +0200

    OpenLayers map insted of google, stil (dissapire in mobile) and shadow

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
 // Initialize and add the map
-function initMap() {
-  // Your location
-  const loc = { lat: 50.443895, lng: 30.546343 };
-  // Centered map on location
-  const map = new google.maps.Map(document.querySelector('.map'), {
-    zoom: 15,
-    center: loc
-  });
-  // The marker, positioned at location
-  const marker = new google.maps.Marker({ position: loc, map: map });
-}
+// function initMap() {
+//   // Your location
+//   const loc = { lat: 50.443895, lng: 30.546343 };
+//   // Centered map on location
+//   const map = new google.maps.Map(document.querySelector('.map'), {
+//     zoom: 15,
+//     center: loc
+//   });
+//   // The marker, positioned at location
+//   const marker = new google.maps.Marker({ position: loc, map: map });
+// }
+
+// OpenLayers map
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([30.51, 50.46]),
+    zoom: 17
+  })
+});
+
 
 // Sticky menu background
 window.addEventListener('scroll', function() {
